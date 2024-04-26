a4e22e6aefbdbffc8d83daff18d29eb5345d0618
plastikup <zachk3yata2@gmail.com>
2024-04-25 20:42:22 -0400

    first commit's unsaved breadcrumbs

--- scripts/snake.js
+++ scripts/snake.js
@@ -5,11 +5,16 @@ export class Snake {
 	constructor(snakeJson, gameSandbox) {
 		this.gameSandbox = gameSandbox;
 		this.snakeJson = snakeJson;
+		this.snakeDirection = 'E';
 	}
 
 	loop() {
-        for (const cell of this.snakeJson) {
-            drawCell(...calculateDimensions(cell.x, cell.y, this.gameSandbox), 's');
-        }
-    }
+		for (const cell of this.snakeJson) {
+			if (cell.head) {
+				drawCell(...calculateDimensions(cell.x, cell.y, this.gameSandbox), 'h' + this.snakeDirection);
+			} else {
+				drawCell(...calculateDimensions(cell.x, cell.y, this.gameSandbox), 's');
+			}
+		}
+	}
 }
