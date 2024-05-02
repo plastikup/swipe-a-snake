3fbbd0d048e5584d4f3fec8d80cf17d745f8cf75
plastikup <zachk3yata2@gmail.com>
2024-05-01 20:43:09 -0400

    added biscuit and collision rules

--- scripts/drawCell.js
+++ scripts/drawCell.js
@@ -4,5 +4,6 @@ import { cellTypesJson } from '../game.js';
 const tilemap = new Image();
 tilemap.src = '../tilemap.png';
 export function drawCell(x, y, size, cellType) {
+	size *= cellTypesJson[cellType].drawingSizeRatio;
 	ctx.drawImage(tilemap, cellTypesJson[cellType].drawingReferenceCell.x * 8, cellTypesJson[cellType].drawingReferenceCell.y * 8, 8, 8, x - size / 2, y - size / 2, size, size);
 }
--- scripts/snake.js
+++ scripts/snake.js
@@ -27,19 +27,22 @@ export class Snake {
 
 	move(panGesture) {
 		if (panGesture !== undefined) {
+			//* collision rules
 			// if cannot pass through, exit
 			const nextX = this.snakeJson[0].x + (panGesture === 0 || panGesture === 2 ? (panGesture === 0 ? 1 : -1) : 0);
 			const nextY = this.snakeJson[0].y + (panGesture === 1 || panGesture === 3 ? (panGesture === 3 ? 1 : -1) : 0);
 			const nextCell = this.gameSandbox.grid[nextY][nextX];
-			if (cellTypesJson[nextCell.cellType].canPassThrough === false || this.snakeJson.filter((e) => e.x === nextX && e.y === nextY).length !== 0) return [undefined, false];
-			// displace the head of the snake
+			if (cellTypesJson[nextCell.cellType].collisionRule === "solid" || this.snakeJson.filter((e) => e.x === nextX && e.y === nextY).length !== 0) return [undefined, false];
+
+			//* displace the head of the snake
 			this.snakeJson[0].head = false;
 			this.snakeJson.unshift({
 				'x': nextX,
 				'y': nextY,
 				'head': true,
 			});
-			// delete the tail of the snake
+
+			//* delete the tail of the snake
 			this.snakeJson.pop();
 
 			console.log(this.snakeJson);
