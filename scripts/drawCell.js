1b7698dd5177e1b91108425919f09dd396f0b43e
plastikup <zachk3yata2@gmail.com>
2024-05-01 21:56:24 -0400

    collectibles' idle motion

--- scripts/drawCell.js
+++ scripts/drawCell.js
@@ -4,6 +4,7 @@ import { cellTypesJson } from '../game.js';
 const tilemap = new Image();
 tilemap.src = '../tilemap.png';
 export function drawCell(x, y, size, cellType) {
-	size *= cellTypesJson[cellType].drawingSizeRatio;
-	ctx.drawImage(tilemap, cellTypesJson[cellType].drawingReferenceCell.x * 8, cellTypesJson[cellType].drawingReferenceCell.y * 8, 8, 8, x - size / 2, y - size / 2, size, size);
+	const cellDictionary = cellTypesJson[cellType];
+	size *= cellDictionary.drawingSizeRatio + (cellDictionary.collisionRule === 'collectible' ? (Math.sin(Date.now() / 400) - 1) * 0.1: 0);
+	ctx.drawImage(tilemap, cellDictionary.drawingReferenceCell.x * 8, cellDictionary.drawingReferenceCell.y * 8, 8, 8, x - size / 2, y - size / 2, size, size);
 }
